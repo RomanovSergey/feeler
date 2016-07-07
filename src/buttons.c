d4be4fde32a1745b46f195b1da9cd35a54238e5b
Romanov Sergey <rsv.981@gmail.com>
2016-07-07 20:50:10 +0300

    add long time button event

--- src/buttons.c
+++ src/buttons.c
@@ -9,21 +9,26 @@
 #include "main.h"
 
 #define READ_B1     GPIO_ReadInputDataBit(GPIOA,GPIO_Pin_8)
-#define ANTI_TIME   30
+#define ANTI_TIME   30    //time of debounce
+#define LPUSH_TIME  1000  //time for long push button event generation
 
 typedef struct  {
-	uint16_t count;//count of acquision (debounce)
+	uint16_t debcount;//count of acquision (debounce)
 	uint8_t  current;//current middle state of Button
+	uint16_t debcurrent;//count of current state of pressed Button
 	uint8_t  prev;//previes middle state of Button
-	uint8_t* ptrPush;//pointer on global - event push (will reset by handler)
+	uint8_t* ptrPush; //pointer on global - event push (will reset by handler)
+	uint8_t* ptrLPush;//pointer on global - event long push (will reset by handler)
 } button_t;
 
 //only one button yet
 button_t B1 = {
-	.count = 0,
+	.debcount = 0,
 	.current = 0,
+	.debcurrent = 0,
 	.prev = 0,
 	.ptrPush = &g.b1_push,
+	.ptrLPush = &g.b1_Lpush,
 };
 
 //================================================================================
@@ -33,24 +38,31 @@ button_t B1 = {
  * to the struct of the button and button instance sensor
  */
 void debounce(button_t *b, uint8_t instance) {
-	if ( instance > 0 ) {//if button is pushed
-		if ( b->count < ANTI_TIME ) {
-			b->count++;//filter
+	if ( instance > 0 ) {//if button is pushed *************************
+		if ( b->debcount < ANTI_TIME ) {
+			b->debcount++;//filter
 		} else {
-			b->current = 1;
 			if ( b->prev == 0 ) {//if state is change
+				b->current = 1;
+				b->debcurrent = 0;
 				b->prev = 1;
 				if ( b->ptrPush != NULL ) {
 					*(b->ptrPush) = 1;//to generate global push button event
 				}
+			} else if ( b->debcurrent > LPUSH_TIME ) {//if time of push exceed long time
+				if ( b->ptrLPush != NULL ) {
+					*(b->ptrLPush) = 1;//to generate global long push button event
+				}
+			} else {
+				b->debcurrent++;
 			}
 		}
-	} else {//if button is pulled
-		if ( b->count > 0 ) {
-			b->count--;//filter
+	} else {//if button is pulled **************************************
+		if ( b->debcount > 0 ) {
+			b->debcount--;//filter
 		} else {
-			b->current = 0;
 			if ( b->prev == 1 ) {//if state is change
+				b->current = 0;
 				b->prev = 0;
 				//yet no task to generate global pull event
 			}

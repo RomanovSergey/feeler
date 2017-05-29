8eecd7d2144e821b56c221f5cdd6e7c5dde0d5b6
Romanov Sergey <rsv.981@gmail.com>
2017-05-29 20:03:25 +0300

    adc jitter div4

--- src/adc.c
+++ src/adc.c
@@ -7,11 +7,15 @@
 
 #include "stm32f0xx.h"
 #include "adc.h"
+#include "uart.h"
 
-static uint16_t calData = 0;
+static uint32_t calData = 0;
 
 void adcSaveCalibData(uint32_t cal)
 {
 	calData = cal;
+	urtPrint("ADC calib: ");
+	urt_uint32_to_str ( calData );
+	urtPrint("\n");
 }
 
